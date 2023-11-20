44d1ee9a84a774a3d26e175de688c0eb94131201
Carolina Mattos <mattos.carolina98@gmail.com>
2023-11-19 21:03:09 -0300

    first commit

---
+++ cypress/e2e/agendaDeContatos/cadastro.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" /> 
+
+describe('Testes para agenda de contatos', () => {
+    beforeEach(() => {
+        cy.visit('https://agenda-contatos-react.vercel.app/')
+    })
+    it('Deve incluir um novo contato a lista', () => {
+        cy.get('input[type="text"').type('Carolina Mattos')
+        cy.get('input[type="email"').type('mattos@gmail.com')
+        cy.get('input[type="tel"').type('11 123456789')
+        cy.get('.adicionar').click()
+        cy.get('.contato').should('have.length', 1)
+    })
+    it('Deve alterar o contato', () => {
+        cy.get('.edit').click()
+        cy.get('input[type="text"').type('Carolina Mattos do N.')
+        cy.get('.alterar').click()
+        cy.get('li').should('contain', 'Carolina Mattos do N.')
+    })
+    it('Deve deletar o contato', () => {
+        cy.get('.delete').click()
+        cy.get('.contato').should('have.length', 0)
+    })
+})
